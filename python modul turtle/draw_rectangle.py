cc07ad8b41f78c5cd05f5a8a2e8b0645ac85505e
DanutaGagua <49496698+DanutaGagua@users.noreply.github.com>
2022-12-31 12:09:41 +0300

    Create draw_rectangle.py

---
+++ python modul turtle/draw_rectangle.py
@@ -0,0 +1,13 @@
+import turtle
+
+def rectangle(width, height):
+    turtle.forward(width)
+    turtle.right(90)
+    turtle.forward(height)
+    turtle.right(90)
+    turtle.forward(width)
+    turtle.right(90)
+    turtle.forward(height)
+    turtle.right(90)
+    
+rectangle(100, 60)
